4d0427562ef48b1388f6f9e6b093c4c28dc2eb1e
Mikazuki <mikazuki_fuyuno@outlook.com>
2020-03-16 23:14:53 +0900

    feat(backend): create Docker instance as singleton

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,20 +1,25 @@
-use actix_web::{middleware, web, App, HttpServer};
 use actix_web::{middleware, App, HttpServer};
+use bollard::Docker;
 use dotenv;
 
 mod routings;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    let port = std::env::var("ARTERIA_PORT").unwrap();
     dotenv::dotenv().ok();
+    env_logger::init();
+
     let server_bind = std::env::var("ARTERIA_BIND").unwrap_or("127.0.0.1".to_string());
     let server_port = std::env::var("ARTERIA_PORT").unwrap_or("3000".to_string());
+    // docker connection
+    let docker =
+        Docker::connect_with_local_defaults().expect("Failed to create a connection with Docker");
 
-    HttpServer::new(|| {
+
+    HttpServer::new(move || {
         App::new()
+            .data(docker.clone())
             .wrap(middleware::Logger::default())
-            .route("/version", web::get().to(routings::version))
             .service(routings::index)
             .service(routings::version)
     })
