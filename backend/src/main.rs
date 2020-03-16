8619e3af09ab333e237c958f4513f9c68a5bc414
Mikazuki <mikazuki_fuyuno@outlook.com>
2020-03-16 23:14:00 +0900

    feat(backend): use dotenv and get bind from env variables

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,11 +1,15 @@
 use actix_web::{middleware, web, App, HttpServer};
 use actix_web::{middleware, App, HttpServer};
+use dotenv;
 
 mod routings;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     let port = std::env::var("ARTERIA_PORT").unwrap();
+    dotenv::dotenv().ok();
+    let server_bind = std::env::var("ARTERIA_BIND").unwrap_or("127.0.0.1".to_string());
+    let server_port = std::env::var("ARTERIA_PORT").unwrap_or("3000".to_string());
 
     HttpServer::new(|| {
         App::new()
@@ -14,7 +18,7 @@ async fn main() -> std::io::Result<()> {
             .service(routings::index)
             .service(routings::version)
     })
-    .bind(format!("127.0.0.1:{}", port))?
+    .bind(format!("{}:{}", server_bind, server_port))?
     .run()
     .await
 }
