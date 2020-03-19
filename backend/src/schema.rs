7a01f59685c19ee67811f326f054bc06e3d37fd6
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2020-03-19 14:08:15 +0900

    feat(backend): create schema

--- backend/src/schema.rs
+++ backend/src/schema.rs
@@ -1,7 +1,40 @@
 table! {
+    dependencies (id) {
+        id -> Bigint,
+        instance_id -> Bigint,
+        name_with_version -> Varchar,
+    }
+}
+
+table! {
+    files (id) {
+        id -> Bigint,
+        instance_id -> Bigint,
+        title -> Varchar,
+        content -> Text,
+    }
+}
+
+table! {
+    instances (id) {
+        id -> Bigint,
+        title -> Varchar,
+        version_id -> Integer,
+        status -> crate::models::InstanceStatusMapping,
+        result -> Nullable<Text>,
+    }
+}
+
+table! {
     versions (id) {
         id -> Integer,
         name -> Varchar,
         tag -> Varchar,
     }
 }
+
+joinable!(dependencies -> instances (instance_id));
+joinable!(files -> instances (instance_id));
+joinable!(instances -> versions (version_id));
+
+allow_tables_to_appear_in_same_query!(dependencies, files, instances, versions,);
