b8a4239e3ea8fbf12e947fe7b9e56d5e1c6b7d1d
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2020-03-19 14:09:27 +0900

    feat(backend): impl insert for dependency model

--- backend/src/models/dependency.rs
+++ backend/src/models/dependency.rs
@@ -15,3 +15,34 @@ pub struct Dependency {
   pub instance_id: i64,
   pub name_with_version: String,
 }
+
+#[derive(Clone, Debug, Insertable)]
+#[table_name = "dependencies"]
+pub struct NewDependency<'a> {
+  pub instance_id: i64,
+  pub name_with_version: &'a str,
+}
+
+impl Dependency {
+  pub fn insert(
+    conn: &MysqlConnection,
+    items: Vec<NewDependency>,
+  ) -> Result<Vec<Dependency>, diesel::result::Error> {
+    use crate::schema::dependencies::dsl::*;
+
+    let items = conn.transaction::<_, diesel::result::Error, _>(|| {
+      let inserted_count = diesel::insert_into(dependencies)
+        .values(items)
+        .execute(conn)?;
+
+      Ok(
+        dependencies
+          .order(id.desc())
+          .limit(inserted_count as i64)
+          .get_results(conn)?,
+      )
+    })?;
+
+    Ok(items)
+  }
+}
