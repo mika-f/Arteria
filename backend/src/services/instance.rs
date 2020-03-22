df3df7fd2bad15f02c1038387badfb3713265038
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2020-03-22 22:16:46 +0900

    feat(backend): returns execution from service

--- backend/src/services/instance.rs
+++ backend/src/services/instance.rs
@@ -2,21 +2,27 @@ use actix::*;
 use actix_web::Error;
 
 use crate::database::dependency::FetchDependenciesByInstanceId;
+use crate::database::executor::FetchExecutorByTag;
 use crate::database::file::FetchFilesByInstanceId;
 use crate::database::instance::FetchInstance;
 use crate::database::transaction::CreateNewInstance;
 use crate::database::DbExecutor;
 use crate::errors;
-use crate::models::{InstanceRequest, InstanceResponse};
+use crate::models::{Executor, InstanceRequest, InstanceResponse};
 
 pub async fn create_instance(
   database: Addr<DbExecutor>,
   object: InstanceRequest,
-) -> Result<(i64, InstanceResponse), Error> {
-  let instance_id = database.send(CreateNewInstance::new(object)).await??;
-  let instance = fetch_instance(database, instance_id).await?;
+) -> Result<(i64, InstanceResponse, Executor), Error> {
+  let instance_id = database
+    .send(CreateNewInstance::new(object.clone()))
+    .await??;
+  let instance = fetch_instance(database.clone(), instance_id).await?;
+  let executor = database
+    .send(FetchExecutorByTag::new(object.executor.to_owned()))
+    .await??;
 
-  Ok((instance_id, instance))
+  Ok((instance_id, instance, executor.unwrap()))
 }
 
 pub async fn fetch_instance(
