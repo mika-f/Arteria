e04de022c9c44a25c8eada4f054243954467638f
Mikazuki <mikazuki_fuyuno@outlook.com>
2020-04-10 19:12:29 +0900

    chore(frontend): use theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Helmet } from "react-helmet";
 import { BrowserRouter, Route } from "react-router-dom";
 
+import withEditorTheme from "./components/hoc/withEditorTheme";
 import withTracker from "./components/hoc/withTracker";
 import About from "./components/pages/About";
 import Privacy from "./components/pages/Privacy";
@@ -13,7 +14,7 @@ const App: React.FC = () => {
     <>
       <Helmet defaultTitle="Arteria" titleTemplate="%s | Arteria" />
       <BrowserRouter>
-        <Route path="/" component={withTracker(Root)} exact />
+        <Route path="/" component={withTracker(withEditorTheme(Root))} exact />
         <Route path="/about" component={withTracker(About)} exact />
         <Route path="/privacy" component={withTracker(Privacy)} exact />
         <Route path="/terms" component={withTracker(Terms)} exact />
---
+++ frontend/src/components/hoc/withEditorTheme.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { LanguageProvider, IconProvider, FileIcon, FileType, ThemeProvider } from "@mika-f/monaka";
+import { Theme } from "@mika-f/monaka/dist/components/ThemeProvider";
+
+const icons = [{}] as FileIcon[];
+
+const languages = [
+  {
+    extension: /\.pl$/,
+    language: "perl"
+  },
+  {
+    extension: /\.pm$/,
+    language: "perl"
+  },
+  {
+    extension: /\.t$/,
+    language: "perl"
+  }
+] as FileType[];
+
+const theme = {
+  base: "vs-dark",
+  activeBackground: "#1e1e1e",
+  editorBackground: "#3c3c3c",
+  errorBackground: "#94124e",
+  inactiveBackground: "#2d2d2d",
+  highlightBackground: "#094771",
+  hoverBackground: "#2a2d2e",
+  background: "#252526",
+  activeBorderColor: "#094771",
+  errorBorderColor: "#ff3d71",
+  fontColor: "#ccc",
+  activeFontColor: "#fff",
+  errorFontColor: "#fff"
+} as Theme;
+
+const withEditorTheme = (WrappedComponent: React.ComponentType<any>): React.FC => {
+  const Hoc: React.FC<any> = props => {
+    return (
+      <LanguageProvider languages={languages}>
+        <IconProvider icons={icons}>
+          <ThemeProvider theme={theme}>
+            {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+            <WrappedComponent {...props} />
+          </ThemeProvider>
+        </IconProvider>
+      </LanguageProvider>
+    );
+  };
+
+  return Hoc;
+};
+
+export default withEditorTheme;
